75ae7f6ae7b4cb59a087de6a71579b004a5b159a
Pegoku <pegoku08@gmail.com>
2025-04-16 09:09:23 +0200

    MainMenu done

--- mainmenu.sh
+++ mainmenu.sh
@@ -1,3 +1,5 @@
+buttons=("BashJack" "BashLots" "RuleBash" "Exit")
+selected=0
 draw_ui() {
     clear
     tput cup 1 10
@@ -19,3 +21,39 @@ draw_ui() {
         fi
     done
 }
+
+
+while true; do
+    draw_ui
+
+    read -rsn1 key
+    case $key in
+        $'\x1b')
+            read -rsn2 -t 0.1 key
+            case $key in
+                '[C') [ $selected -lt 3 ] && selected=$((selected + 1)) ;;
+                '[D') [ $selected -gt 0 ] && selected=$((selected - 1)) ;;
+            esac
+            ;;
+        '')
+            if [ $selected -eq 0 ]; then
+                # Start BashJack game
+                clear
+                bash ./BashJack/main.sh
+            elif [ $selected -eq 1 ]; then
+                # Start BashLots game.
+                clear
+                bash ./BashLots/main.sh
+            elif [ $selected -eq 2 ]; then
+                # Show rules
+                clear
+                bash ./RuleBash/main.sh
+            elif [ $selected -eq 3 ]; then
+                # Exit the script
+                clear
+                exit 0
+            fi
+            ;;
+    esac
+done
+done
